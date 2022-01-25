6ca1903a7f59c80dc06f1ba3e2905e67a2f1ca35
tellang <tellang@daum.net>
2022-01-25 15:10:56 +0900

    기본세팅

---
+++ src/test/java/kr/reciptopia/reciptopiaserver/ReciptopiaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.reciptopia.reciptopiaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciptopiaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
