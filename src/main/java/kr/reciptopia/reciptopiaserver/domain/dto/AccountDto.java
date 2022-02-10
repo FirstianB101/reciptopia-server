94b2328fdc90a3e504fd15832803b95b71764b69
tellang <tellang@daum.net>
2022-02-10 14:41:01 +0900

    AccountDto추가

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/AccountDto.java
@@ -0,0 +1,73 @@
+package kr.reciptopia.reciptopiaserver.domain.dto;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import lombok.Builder;
+import lombok.Data;
+import lombok.With;
+
+public interface AccountDto {
+
+    @Data
+    @Builder
+    @With
+    class Create {
+
+        @NotNull
+        @Email(message = "이메일 형식이 아닙니다.")
+        private String email;
+
+        @NotEmpty
+        @Size(min = 8, max = 16, message = "password는 8 ~ 16자 이여야 합니다!")
+        private String password;
+
+        @NotBlank
+        @Size(min = 5, max = 16, message = "nickname은 5 ~ 16자 이여야 합니다!")
+        private String nickname;
+    }
+
+    @Data
+    @Builder
+    @With
+    class Update {
+
+        @Email(message = "이메일 형식이 아닙니다.")
+        private String email;
+
+        @Size(min = 8, max = 16, message = "password는 8 ~ 16자 이여야 합니다!")
+        private String password;
+
+        @Size(min = 5, max = 16, message = "nickname은 5 ~ 16자 이여야 합니다!")
+        private String nickname;
+
+        private String profilePictureUrl;
+    }
+
+    @Data
+    @Builder
+    @With
+    class Result {
+
+        @NotNull
+        @Email(message = "이메일 형식이 아닙니다.")
+        private String email;
+
+        @NotBlank
+        @Size(min = 5, max = 16, message = "nickname은 5 ~ 16자 이여야 합니다!")
+        private String nickname;
+
+        private String profilePictureUrl;
+    }
+
+    @Data
+    @Builder
+    @With
+    class CheckDuplicationResult {
+
+        @NotNull
+        private Boolean exists;
+    }
+}
