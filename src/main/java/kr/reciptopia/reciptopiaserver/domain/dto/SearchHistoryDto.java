4172147eb673f14f0b2c6c2ee8d902b27e12b6cc
Kim EunSung <dmstjd365@naver.com>
2022-03-13 18:04:37 +0900

    `SearchHistoryDto` 추가

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/SearchHistoryDto.java
@@ -0,0 +1,46 @@
+package kr.reciptopia.reciptopiaserver.domain.dto;
+
+import kr.reciptopia.reciptopiaserver.domain.model.SearchHistory;
+import lombok.Builder;
+import lombok.Data;
+import lombok.With;
+
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+public interface SearchHistoryDto {
+
+	static Result of(SearchHistory entity) {
+		return Result.builder()
+				.ownerId(entity.getOwner().getId())
+				.ingredients(entity.getIngredients())
+				.recipeName(entity.getRecipeName())
+				.build();
+	}
+
+	@Data
+	@Builder
+	@With
+	class Create {
+
+		@NotNull
+		private Long ownerId;
+
+		private Set<String> ingredients;
+
+		private String recipeName;
+	}
+
+	@Data
+	@Builder
+	@With
+	class Result {
+
+		@NotNull
+		private Long ownerId;
+
+		private Set<String> ingredients;
+
+		private String recipeName;
+	}
+}
