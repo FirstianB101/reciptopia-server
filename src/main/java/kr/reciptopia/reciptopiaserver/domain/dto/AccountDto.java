77efa1739e43d49bd6369b50e9edf08173bc0e52
tellang <tellang@daum.net>
2022-03-15 14:09:34 +0900

    AccountDto.Result에 다중변환 메소드 오버로딩
    `Paging`을 위한 다중 `DTO` <-> `Entity`간의 변환 메소드 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/AccountDto.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/AccountDto.java
@@ -1,5 +1,7 @@
 package kr.reciptopia.reciptopiaserver.domain.dto;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
@@ -10,6 +12,7 @@ import kr.reciptopia.reciptopiaserver.domain.model.UserRole;
 import lombok.Builder;
 import lombok.Data;
 import lombok.With;
+import org.springframework.data.util.Streamable;
 
 public interface AccountDto {
 
@@ -85,6 +88,12 @@ public interface AccountDto {
                 .profilePictureUrl(entitiy.getProfilePictureUrl())
                 .role(entitiy.getRole())
                 .build();
+        }
+
+        public static List<Result> of(Streamable<Account> entities) {
+            return entities.stream()
+                .map(Result::of)
+                .collect(Collectors.toList());
         }
     }
 
