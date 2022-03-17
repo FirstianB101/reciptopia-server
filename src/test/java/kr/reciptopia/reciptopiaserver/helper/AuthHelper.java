42657cf8bcba603d7c3d0f301376c93253c3136b
tellang <tellang@daum.net>
2022-03-17 18:41:52 +0900

    recode와 맞지 않은 애너테이션 제거
    recode는 모든 필드를 초기화하는 RequiredAllArgument 생성자가 생성되므로 해당 애너테이션은 필요없다

--- src/test/java/kr/reciptopia/reciptopiaserver/helper/AuthHelper.java
+++ src/test/java/kr/reciptopia/reciptopiaserver/helper/AuthHelper.java
@@ -3,10 +3,8 @@ package kr.reciptopia.reciptopiaserver.helper;
 
 import kr.reciptopia.reciptopiaserver.business.service.JwtService;
 import kr.reciptopia.reciptopiaserver.domain.model.Account;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
-@AllArgsConstructor
 @Component
 public record AuthHelper(JwtService jwtService) {
 
