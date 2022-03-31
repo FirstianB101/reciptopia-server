0db6c9706d8a7c63964f27bbb123fc8596ab7119
Kim EunSung <dmstjd365@naver.com>
2022-03-31 22:16:39 +0900

    코드 리팩토링

--- src/main/java/kr/reciptopia/reciptopiaserver/business/service/PostService.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/business/service/PostService.java
@@ -3,7 +3,6 @@ package kr.reciptopia.reciptopiaserver.business.service;
 import java.util.List;
 import kr.reciptopia.reciptopiaserver.business.service.authorizer.AbstractAuthorizer;
 import kr.reciptopia.reciptopiaserver.business.service.helper.RepositoryHelper;
-import kr.reciptopia.reciptopiaserver.business.service.helper.ServiceErrorHelper;
 import kr.reciptopia.reciptopiaserver.domain.dto.PostDto.Create;
 import kr.reciptopia.reciptopiaserver.domain.dto.PostDto.Result;
 import kr.reciptopia.reciptopiaserver.domain.dto.PostDto.Update;
@@ -15,7 +14,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,75 +21,70 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class PostService {
 
-	private final PasswordEncoder passwordEncoder;
-	private final PostRepository postRepository;
-	private final RepositoryHelper repoHelper;
-	private final ServiceErrorHelper errorHelper;
-	private final AbstractAuthorizer authorizer;
+    private final PostRepository postRepository;
+    private final RepositoryHelper repoHelper;
+    private final AbstractAuthorizer authorizer;
 
-	@Autowired
+    @Autowired
 
-	public PostService(PasswordEncoder passwordEncoder,
-		PostRepository postRepository,
-		RepositoryHelper repoHelper,
-		ServiceErrorHelper errorHelper,
-		AbstractAuthorizer authorizer) {
-		this.passwordEncoder = passwordEncoder;
-		this.postRepository = postRepository;
-		this.repoHelper = repoHelper;
-		this.errorHelper = errorHelper;
-		this.authorizer = authorizer;
-	}
+    public PostService(
+        PostRepository postRepository,
+        RepositoryHelper repoHelper,
+        AbstractAuthorizer authorizer) {
+        this.postRepository = postRepository;
+        this.repoHelper = repoHelper;
+        this.authorizer = authorizer;
+    }
 
-	@Transactional
-	public Result create(Create dto, Authentication authentication) {
-		Post post = dto.asEntity();
+    @Transactional
+    public Result create(Create dto, Authentication authentication) {
+        Post post = dto.asEntity();
 
-		Account dtoOwner = repoHelper.findAccountOrThrow(dto.ownerId());
-		authorizer.requireByOneself(authentication, dtoOwner);
+        Account dtoOwner = repoHelper.findAccountOrThrow(dto.ownerId());
+        authorizer.requireByOneself(authentication, dtoOwner);
 
-		post.setOwner(dtoOwner);
+        post.setOwner(dtoOwner);
 
 //		Recipe dtoRecipe = repoHelper.findRecipeOrThrow(dto.getRecipeId());
 //
 //		post.setRecipe(dtoRecipe);
 
-		return Result.of(postRepository.save(post));
-	}
+        return Result.of(postRepository.save(post));
+    }
 
-	public Result read(Long id) {
-		return Result.of(repoHelper.findPostOrThrow(id));
-	}
+    public Result read(Long id) {
+        return Result.of(repoHelper.findPostOrThrow(id));
+    }
 
-	public List<Result> search(Specification<Post> spec, Pageable pageable) {
-		Page<Post> entities = postRepository.findAll(spec, pageable);
-		return Result.of(entities);
-	}
+    public List<Result> search(Specification<Post> spec, Pageable pageable) {
+        Page<Post> entities = postRepository.findAll(spec, pageable);
+        return Result.of(entities);
+    }
 
-	@Transactional
-	public Result update(Long id, Update dto, Authentication authentication) {
-		Post entity = repoHelper.findPostOrThrow(id);
-		authorizer.requireByOneself(authentication, entity.getOwner());
+    @Transactional
+    public Result update(Long id, Update dto, Authentication authentication) {
+        Post entity = repoHelper.findPostOrThrow(id);
+        authorizer.requireByOneself(authentication, entity.getOwner());
 
-		if (dto.pictureUrls() != null) {
-			entity.setPictureUrls(dto.pictureUrls());
-		}
-		if (dto.title() != null) {
-			entity.setTitle(dto.title());
-		}
-		if (dto.content() != null) {
-			entity.setContent(dto.content());
-		}
+        if (dto.pictureUrls() != null) {
+            entity.setPictureUrls(dto.pictureUrls());
+        }
+        if (dto.title() != null) {
+            entity.setTitle(dto.title());
+        }
+        if (dto.content() != null) {
+            entity.setContent(dto.content());
+        }
 
-		return Result.of(postRepository.save(entity));
-	}
+        return Result.of(postRepository.save(entity));
+    }
 
-	@Transactional
-	public void delete(Long id, Authentication authentication) {
-		Post post = repoHelper.findPostOrThrow(id);
-		authorizer.requireByOneself(authentication, post.getOwner());
-		post.removeAllCollections();
+    @Transactional
+    public void delete(Long id, Authentication authentication) {
+        Post post = repoHelper.findPostOrThrow(id);
+        authorizer.requireByOneself(authentication, post.getOwner());
+        post.removeAllCollections();
 
-		postRepository.delete(post);
-	}
+        postRepository.delete(post);
+    }
 }
--- src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/PostDto.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/PostDto.java
@@ -12,108 +12,108 @@ import org.springframework.data.util.Streamable;
 
 public interface PostDto {
 
-	@With
-	record Create(Long ownerId, Long recipeId,
-				  String title, String content, List<String> pictureUrls) {
+    @With
+    record Create(Long ownerId, Long recipeId,
+                  String title, String content, List<String> pictureUrls) {
 
-		@Builder
-		public Create(
-			@NotNull
-				Long ownerId,
+        @Builder
+        public Create(
+            @NotNull
+                Long ownerId,
 
-			@NotNull
-				Long recipeId,
+            @NotNull
+                Long recipeId,
 
-			@NotEmpty
-				String title,
+            @NotEmpty
+                String title,
 
-			String content,
+            String content,
 
-			@Singular
-				List<String> pictureUrls) {
-			this.ownerId = ownerId;
-			this.recipeId = recipeId;
-			this.title = title;
-			this.content = content;
-			this.pictureUrls = pictureUrls;
-		}
+            @Singular
+                List<String> pictureUrls) {
+            this.ownerId = ownerId;
+            this.recipeId = recipeId;
+            this.title = title;
+            this.content = content;
+            this.pictureUrls = pictureUrls;
+        }
 
-		public Post asEntity() {
-			return Post.builder()
-				.title(title)
-				.content(content)
-				.pictureUrls(pictureUrls)
-				.build();
-		}
-	}
+        public Post asEntity() {
+            return Post.builder()
+                .title(title)
+                .content(content)
+                .pictureUrls(pictureUrls)
+                .build();
+        }
+    }
 
-	@With
-	record Update(String title, String content, List<String> pictureUrls) {
+    @With
+    record Update(String title, String content, List<String> pictureUrls) {
 
-		@Builder
-		public Update(
-			@NotEmpty
-				String title,
+        @Builder
+        public Update(
+            @NotEmpty
+                String title,
 
-			String content,
+            String content,
 
-			@Singular
-				List<String> pictureUrls) {
-			this.title = title;
-			this.content = content;
-			this.pictureUrls = pictureUrls;
-		}
-	}
+            @Singular
+                List<String> pictureUrls) {
+            this.title = title;
+            this.content = content;
+            this.pictureUrls = pictureUrls;
+        }
+    }
 
-	@With
-	record Result(
-		Long id, Long ownerId, Long recipeId,
-		String title, String content, List<String> pictureUrls, Long views) {
+    @With
+    record Result(
+        Long id, Long ownerId, Long recipeId,
+        String title, String content, List<String> pictureUrls, Long views) {
 
-		@Builder
-		public Result(
-			@NotNull
-				Long id,
+        @Builder
+        public Result(
+            @NotNull
+                Long id,
 
-			@NotNull
-				Long ownerId,
+            @NotNull
+                Long ownerId,
 
-			@NotNull
-				Long recipeId,
+            @NotNull
+                Long recipeId,
 
-			@NotEmpty
-				String title,
+            @NotEmpty
+                String title,
 
-			String content,
+            String content,
 
-			List<String> pictureUrls,
+            List<String> pictureUrls,
 
-			Long views) {
-			this.id = id;
-			this.ownerId = ownerId;
-			this.recipeId = recipeId;
-			this.title = title;
-			this.content = content;
-			this.pictureUrls = pictureUrls;
-			this.views = views;
-		}
+            Long views) {
+            this.id = id;
+            this.ownerId = ownerId;
+            this.recipeId = recipeId;
+            this.title = title;
+            this.content = content;
+            this.pictureUrls = pictureUrls;
+            this.views = views;
+        }
 
-		public static Result of(Post entity) {
-			return Result.builder()
-				.id(entity.getId())
-				.ownerId(entity.getOwner().getId())
+        public static Result of(Post entity) {
+            return Result.builder()
+                .id(entity.getId())
+                .ownerId(entity.getOwner().getId())
 //					.recipeId(entity.getRecipe().getId())
-				.title(entity.getTitle())
-				.content(entity.getContent())
-				.pictureUrls(entity.getPictureUrls())
-				.views(entity.getViews())
-				.build();
-		}
+                .title(entity.getTitle())
+                .content(entity.getContent())
+                .pictureUrls(entity.getPictureUrls())
+                .views(entity.getViews())
+                .build();
+        }
 
-		public static List<Result> of(Streamable<Post> entities) {
-			return entities.stream()
-				.map(post -> of(post))
-				.collect(Collectors.toList());
-		}
-	}
+        public static List<Result> of(Streamable<Post> entities) {
+            return entities.stream()
+                .map(post -> of(post))
+                .collect(Collectors.toList());
+        }
+    }
 }
