d37db48b5487a39f204a53b8fdecbeedd7d5e008
Kim EunSung <dmstjd365@naver.com>
2022-04-02 17:47:30 +0900

    테스트시 Comment 생성을 위한 CommentHelper 구현 및 EntityHelper에 Comment 관련 메소드 추가

--- src/test/java/kr/reciptopia/reciptopiaserver/helper/EntityHelper.java
+++ src/test/java/kr/reciptopia/reciptopiaserver/helper/EntityHelper.java
@@ -1,12 +1,13 @@
 package kr.reciptopia.reciptopiaserver.helper;
 
 import static kr.reciptopia.reciptopiaserver.helper.AccountHelper.anAccount;
+import static kr.reciptopia.reciptopiaserver.helper.CommentHelper.aComment;
 import static kr.reciptopia.reciptopiaserver.helper.PostHelper.aPost;
-
 import java.util.ArrayList;
 import java.util.function.Function;
 import javax.persistence.EntityManager;
 import kr.reciptopia.reciptopiaserver.domain.model.Account;
+import kr.reciptopia.reciptopiaserver.domain.model.Comment;
 import kr.reciptopia.reciptopiaserver.domain.model.Post;
 import org.springframework.stereotype.Component;
 
@@ -47,6 +48,28 @@ public record EntityHelper(EntityManager em) {
 
         em.persist(post);
         return post;
+    }
+
+    public Comment generateComment() {
+        return generateComment(noInit());
+    }
+
+    public Comment generateComment(Function<? super Comment, ? extends Comment> initialize) {
+        Comment comment = aComment()
+            .withId(null)
+            .withOwner(null)
+            .withPost(null);
+
+        comment = initialize.apply(comment);
+        if (comment.getOwner() == null) {
+            comment.setOwner(generateAccount());
+        }
+        if (comment.getPost() == null) {
+            comment.setPost(generatePost());
+        }
+
+        em.persist(comment);
+        return comment;
     }
 
     private <T> Function<? super T, ? extends T> noInit() {
