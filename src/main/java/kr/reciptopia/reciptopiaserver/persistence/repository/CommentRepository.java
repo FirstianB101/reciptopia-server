702d591f57abf03c26f85266180f52d1571cf0d6
Kim EunSung <dmstjd365@naver.com>
2022-04-03 15:43:16 +0900

    CommentService에서 사용하지 않는 리포지토리의 메소드 삭제

--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/CommentRepository.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/CommentRepository.java
@@ -1,12 +1,8 @@
 package kr.reciptopia.reciptopiaserver.persistence.repository;
 
-import java.util.Optional;
 import kr.reciptopia.reciptopiaserver.domain.model.Comment;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CommentRepository extends BaseRepository<Comment, Long> {
-
-    Optional<Comment> findByOwnerIdAndPostId(Long ownerId, Long postId);
-
 }
