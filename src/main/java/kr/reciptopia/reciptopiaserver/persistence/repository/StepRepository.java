4df78a43c08e36d142e12be19a40b5d8f267f5ca
tellang <tellang@daum.net>
2022-04-05 16:53:45 +0900

    StepRepository 추가
    추후 필요한 쿼리에 따라 메소드를 추가할 예정

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/StepRepository.java
@@ -0,0 +1,9 @@
+package kr.reciptopia.reciptopiaserver.persistence.repository;
+
+import kr.reciptopia.reciptopiaserver.domain.model.Step;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StepRepository extends BaseRepository<Step, Long> {
+
+}
