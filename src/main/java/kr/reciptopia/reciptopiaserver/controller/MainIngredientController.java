e71c8e5c6b089d4da58362de78f94203acef913e
tellang <tellang@daum.net>
2022-04-06 14:26:36 +0900

    MainIngredientController추가

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/controller/MainIngredientController.java
@@ -0,0 +1,52 @@
+package kr.reciptopia.reciptopiaserver.controller;
+
+import static kr.reciptopia.reciptopiaserver.domain.dto.MainIngredientDto.Create;
+import static kr.reciptopia.reciptopiaserver.domain.dto.MainIngredientDto.Result;
+import static kr.reciptopia.reciptopiaserver.domain.dto.MainIngredientDto.Update;
+
+import javax.validation.Valid;
+import kr.reciptopia.reciptopiaserver.business.service.MainIngredientService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@Validated
+@RestController
+@RequiredArgsConstructor
+public class MainIngredientController {
+
+    private final MainIngredientService service;
+
+    @PostMapping("/post/recipe/mainIngredients")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Result post(@Valid @RequestBody Create dto,
+        Authentication authentication) {
+        return service.create(dto, authentication);
+    }
+
+    @GetMapping("/post/recipe/mainIngredients/{id}")
+    public Result get(@PathVariable Long id) {
+        return service.read(id);
+    }
+
+    @PatchMapping("/post/recipe/mainIngredients/{id}")
+    public Result patch(@PathVariable Long id,
+        @Valid @RequestBody Update dto, Authentication authentication) {
+        return service.update(id, dto, authentication);
+    }
+
+    @DeleteMapping("/post/recipe/mainIngredients/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id, Authentication authentication) {
+        service.delete(id, authentication);
+    }
+}
