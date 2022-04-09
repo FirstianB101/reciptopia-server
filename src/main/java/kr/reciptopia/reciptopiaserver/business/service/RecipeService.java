55364cfb10ff2e8df1e2ffe1c8aa782e7f71d2e6
tellang <tellang@daum.net>
2022-04-09 15:36:28 +0900

    RecipeService 추가
    `Recipe`는 현재 연관관계를 제외한 필드를 갖고있지 않아서 `Patch`에 대한 `Service`가 존재하지 않음, 추후 필요에따라 추가할 예정

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/business/service/RecipeService.java
@@ -0,0 +1,45 @@
+package kr.reciptopia.reciptopiaserver.business.service;
+
+import static kr.reciptopia.reciptopiaserver.domain.dto.RecipeDto.Create;
+import static kr.reciptopia.reciptopiaserver.domain.dto.RecipeDto.Result;
+
+import kr.reciptopia.reciptopiaserver.business.service.authorizer.RecipeAuthorizer;
+import kr.reciptopia.reciptopiaserver.business.service.helper.RepositoryHelper;
+import kr.reciptopia.reciptopiaserver.domain.model.Post;
+import kr.reciptopia.reciptopiaserver.domain.model.Recipe;
+import kr.reciptopia.reciptopiaserver.persistence.repository.RecipeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class RecipeService {
+
+    private final RecipeRepository RecipeRepository;
+    private final RepositoryHelper repoHelper;
+    private final RecipeAuthorizer recipeAuthorizer;
+
+    @Transactional
+    public Result create(Create dto, Authentication authentication) {
+        Post post = repoHelper.findPostOrThrow(dto.postId());
+        recipeAuthorizer.requirePostOwner(authentication, post);
+
+        Recipe recipe = dto.asEntity().withPost(post);
+
+        return Result.of(RecipeRepository.save(recipe));
+    }
+
+    public Result read(Long id) {
+        return Result.of(repoHelper.findRecipeOrThrow(id));
+    }
+
+    @Transactional
+    public void delete(Long id, Authentication authentication) {
+        Recipe recipe = repoHelper.findRecipeOrThrow(id);
+        recipeAuthorizer.requireRecipeOwner(authentication, recipe);
+        RecipeRepository.delete(recipe);
+    }
+}
