97758254e4afc4863a5f516c17aa7b30b13d854e
tellang <tellang@daum.net>
2022-04-09 12:47:21 +0900

    SubIngredientService 추가

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/business/service/SubIngredientService.java
@@ -0,0 +1,81 @@
+package kr.reciptopia.reciptopiaserver.business.service;
+
+import static kr.reciptopia.reciptopiaserver.domain.dto.SubIngredientDto.Bulk;
+import static kr.reciptopia.reciptopiaserver.domain.dto.SubIngredientDto.Create;
+import static kr.reciptopia.reciptopiaserver.domain.dto.SubIngredientDto.Result;
+import static kr.reciptopia.reciptopiaserver.domain.dto.SubIngredientDto.Update;
+
+import java.util.List;
+import kr.reciptopia.reciptopiaserver.business.service.authorizer.IngredientAuthorizer;
+import kr.reciptopia.reciptopiaserver.business.service.helper.RepositoryHelper;
+import kr.reciptopia.reciptopiaserver.domain.model.Account;
+import kr.reciptopia.reciptopiaserver.domain.model.Recipe;
+import kr.reciptopia.reciptopiaserver.domain.model.SubIngredient;
+import kr.reciptopia.reciptopiaserver.persistence.repository.SubIngredientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SubIngredientService {
+
+    private final SubIngredientRepository subIngredientRepository;
+    private final RepositoryHelper repoHelper;
+    private final IngredientAuthorizer ingredientAuthorizer;
+
+    @Transactional
+    public Result create(Create dto, Authentication authentication) {
+        Recipe recipe = repoHelper.findRecipeOrThrow(dto.recipeId());
+        ingredientAuthorizer.requireRecipeOwner(authentication, recipe);
+
+        SubIngredient subIngredient = dto.asEntity(it -> it.withRecipe(recipe));
+
+        return Result.of(subIngredientRepository.save(subIngredient));
+    }
+
+    public Result read(Long id) {
+        return Result.of(repoHelper.findSubIngredientOrThrow(id));
+    }
+
+    public List<Result> search(Specification<SubIngredient> spec, Pageable pageable) {
+        Page<SubIngredient> subIngredients = subIngredientRepository.findAll(spec, pageable);
+        return Result.of(subIngredients);
+    }
+
+    @Transactional
+    public Result update(Long id, Update dto, Authentication authentication) {
+        SubIngredient subIngredient = repoHelper.findSubIngredientOrThrow(id);
+        Account owner = subIngredient.getRecipe().getPost().getOwner();
+        ingredientAuthorizer.requireByOneself(authentication, owner);
+
+        if (dto.name() != null) {
+            subIngredient.setName(dto.name());
+        }
+        if (dto.detail() != null) {
+            subIngredient.setDetail(dto.detail());
+        }
+
+        return Result.of(subIngredientRepository.save(subIngredient));
+    }
+
+    @Transactional
+    public void delete(Long id, Authentication authentication) {
+        SubIngredient subIngredient = repoHelper.findSubIngredientOrThrow(id);
+        ingredientAuthorizer.requireIngredientOwner(authentication, subIngredient);
+
+        subIngredientRepository.delete(subIngredient);
+    }
+
+    @Transactional
+    public void update(Bulk.Update bulkSubIngredient,
+        Authentication authentication) {
+        bulkSubIngredient.subIngredients().keySet()
+            .forEach(id -> update(id, bulkSubIngredient.subIngredients().get(id), authentication));
+    }
+}
