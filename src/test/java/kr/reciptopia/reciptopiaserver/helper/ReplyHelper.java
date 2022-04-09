742a1dcf41df480491d3f85a83685e4102402ee7
Kim EunSung <dmstjd365@naver.com>
2022-04-09 17:58:59 +0900

    ReplyHelper에서 각 Dto에 임의의 테스트 값을 넣은 후, 반환

--- src/test/java/kr/reciptopia/reciptopiaserver/helper/ReplyHelper.java
+++ src/test/java/kr/reciptopia/reciptopiaserver/helper/ReplyHelper.java
@@ -7,7 +7,10 @@ import kr.reciptopia.reciptopiaserver.domain.model.Reply;
 
 public class ReplyHelper {
 
+    private static final Long ARBITRARY_OWNER_ID = 1L;
+    private static final Long ARBITRARY_COMMENT_ID = 10L;
     private static final String ARBITRARY_CONTENT = "테스트 답글 내용";
+    private static final String NEW_ARBITRARY_CONTENT = "새로운 답글 내용";
 
     public static Reply aReply() {
         return Reply.builder()
@@ -18,16 +21,23 @@ public class ReplyHelper {
 
     public static Create aReplyCreateDto() {
         return Create.builder()
+            .ownerId(ARBITRARY_OWNER_ID)
+            .commentId(ARBITRARY_COMMENT_ID)
+            .content(ARBITRARY_CONTENT)
             .build();
     }
 
     public static Update aReplyUpdateDto() {
         return Update.builder()
+            .content(NEW_ARBITRARY_CONTENT)
             .build();
     }
 
     public static Result aReplyResultDto() {
         return Result.builder()
+            .ownerId(ARBITRARY_OWNER_ID)
+            .commentId(ARBITRARY_COMMENT_ID)
+            .content(ARBITRARY_CONTENT)
             .build();
     }
 
