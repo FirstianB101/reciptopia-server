65954fe1cb1e5274510258c8c6fe5945b7e7658f
tellang <tellang@daum.net>
2022-04-09 12:40:25 +0900

    SubIngredientRepository 추가

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/SubIngredientRepository.java
@@ -0,0 +1,9 @@
+package kr.reciptopia.reciptopiaserver.persistence.repository;
+
+import kr.reciptopia.reciptopiaserver.domain.model.SubIngredient;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubIngredientRepository extends BaseRepository<SubIngredient, Long> {
+
+}
