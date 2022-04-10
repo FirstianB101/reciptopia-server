dfb4c6cf5db8f7651faeaf580776ee0456db23b0
Silver Star <37132563+KimSilverStar@users.noreply.github.com>
2022-04-10 16:00:01 +0900

    CommentDto.Result의 적절하지 않은 필드 삭제 (#107)
    * CommentDto.Result의 필드에 적절하지 않은 필드 삭제
    
    - 특정 엔티티를 조회 시, Dto로 다른 엔티티 자체를 포함하여 반환하는 것은 바람직하지 않음
    - 이에따라 CommentDto.Reulst에 Reply, CommentLikeTag 엔티티를 반환하지 않도록, 해당 필드를 삭제
    
    * CommentController에 불필요한 PostService 필드 삭제
    
    * Comment 통합 테스트 수정
    
    - Comment 조회 테스트에서 Document의 responseFields 수정
    - 불필요한 replies, commentLikeTags에 관한 필드 삭제
    
    * Comment 통합 테스트에서 토큰 생성 수정
    
    - CommentAuthHelper를 이용하여 Comment 객체를 인자로 받도록 수정

--- src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/CommentDto.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/CommentDto.java
@@ -1,16 +1,12 @@
 package kr.reciptopia.reciptopiaserver.domain.dto;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import kr.reciptopia.reciptopiaserver.domain.model.Comment;
-import kr.reciptopia.reciptopiaserver.domain.model.CommentLikeTag;
-import kr.reciptopia.reciptopiaserver.domain.model.Reply;
 import lombok.Builder;
-import lombok.Singular;
 import lombok.With;
 import org.springframework.data.util.Streamable;
 
@@ -57,8 +53,7 @@ public interface CommentDto {
 
     @With
     record Result(
-        Long id, Long ownerId, Long postId, String content,
-        Set<Reply> replies, Set<CommentLikeTag> commentLikeTags) {
+        Long id, Long ownerId, Long postId, String content) {
 
         @Builder
         public Result(
@@ -73,19 +68,11 @@ public interface CommentDto {
 
             @NotBlank
             @Size(min = 1, max = 50, message = "content는 1 ~ 50자 이여야 합니다!")
-                String content,
-
-            @Singular
-                Set<Reply> replies,
-
-            @Singular
-                Set<CommentLikeTag> commentLikeTags) {
+                String content) {
             this.id = id;
             this.ownerId = ownerId;
             this.postId = postId;
             this.content = content;
-            this.replies = replies;
-            this.commentLikeTags = commentLikeTags;
         }
 
         public static Result of(Comment entity) {
@@ -94,8 +81,6 @@ public interface CommentDto {
                 .ownerId(entity.getOwner().getId())
                 .postId(entity.getPost().getId())
                 .content(entity.getContent())
-                .replies(entity.getReplies())
-                .commentLikeTags(entity.getCommentLikeTags())
                 .build();
         }
 
