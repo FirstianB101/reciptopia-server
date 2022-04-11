6f0239072ea2af861c3a61de691ca06f5afaa1a2
Kim EunSung <dmstjd365@naver.com>
2022-04-11 17:10:03 +0900

    CommentHelper에 static 메소드 추가
    - 임의의 CommentDto.Create를 생성하여 반환

--- src/test/java/kr/reciptopia/reciptopiaserver/helper/CommentHelper.java
+++ src/test/java/kr/reciptopia/reciptopiaserver/helper/CommentHelper.java
@@ -2,11 +2,14 @@ package kr.reciptopia.reciptopiaserver.helper;
 
 import static kr.reciptopia.reciptopiaserver.helper.AccountHelper.anAccount;
 import static kr.reciptopia.reciptopiaserver.helper.PostHelper.aPost;
+import kr.reciptopia.reciptopiaserver.domain.dto.CommentDto.Create;
 import kr.reciptopia.reciptopiaserver.domain.dto.CommentDto.Update;
 import kr.reciptopia.reciptopiaserver.domain.model.Comment;
 
 public class CommentHelper {
 
+    private static final Long ARBITRARY_OWNER_ID = 0L;
+    private static final Long ARBITRARY_POST_ID = 0L;
     private static final String ARBITRARY_CONTENT = "테스트 댓글 내용";
 
     public static Comment aComment() {
@@ -17,6 +20,13 @@ public class CommentHelper {
         comment.setOwner(anAccount());
         comment.setPost(aPost());
         return comment;
+    }
+
+    public static Create aCommentCreateDto() {
+        return Create.builder()
+            .ownerId(ARBITRARY_OWNER_ID)
+            .postId(ARBITRARY_POST_ID)
+            .build();
     }
 
     public static Update aCommentUpdateDto() {
