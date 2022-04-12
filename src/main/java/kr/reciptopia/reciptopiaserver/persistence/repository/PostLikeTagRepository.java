dd27da8c9174924d30cc30ddba001d721f17ad83
Kim EunSung <dmstjd365@naver.com>
2022-04-12 22:50:01 +0900

    PostLikeTagRepository 구현

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/PostLikeTagRepository.java
@@ -0,0 +1,9 @@
+package kr.reciptopia.reciptopiaserver.persistence.repository;
+
+import kr.reciptopia.reciptopiaserver.domain.model.PostLikeTag;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostLikeTagRepository extends BaseRepository<PostLikeTag, Long> {
+
+}
