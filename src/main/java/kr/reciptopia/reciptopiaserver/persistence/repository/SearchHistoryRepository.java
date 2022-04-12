5477f4b2a1746a53a608f43864416b21017d7c57
tellang <tellang@daum.net>
2022-04-12 16:58:12 +0900

    SearchHistoryRepository 추가

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/SearchHistoryRepository.java
@@ -0,0 +1,9 @@
+package kr.reciptopia.reciptopiaserver.persistence.repository;
+
+import kr.reciptopia.reciptopiaserver.domain.model.SearchHistory;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SearchHistoryRepository extends BaseRepository<SearchHistory, Long> {
+
+}
