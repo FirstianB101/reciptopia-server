0324065ab5b2e79dc030a21d8d81eca0ade994ef
tellang <tellang@daum.net>
2022-04-12 17:23:24 +0900

    사용되지 않는 애노테이션 제거

--- src/main/java/kr/reciptopia/reciptopiaserver/domain/model/MainIngredient.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/model/MainIngredient.java
@@ -8,13 +8,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import lombok.With;
 
 @Getter
 @Setter
 @ToString
 @Entity
-@With
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MainIngredient extends Ingredient {
 
