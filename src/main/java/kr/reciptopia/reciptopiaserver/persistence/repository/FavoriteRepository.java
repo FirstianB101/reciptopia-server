1e6b5d8b28dbb191cc5e2af4aa88f0f7dfa110e6
tellang <tellang@daum.net>
2022-04-14 20:41:12 +0900

    Account의 id로 각각의 엔티티 삭제하는 메소드 추가
    단방향으로 변경으로 인한 하위 자원 삭제시 상위 자원 삭제기능 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/FavoriteRepository.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/FavoriteRepository.java
@@ -1,10 +1,9 @@
 package kr.reciptopia.reciptopiaserver.persistence.repository;
 
-import java.util.Optional;
 import kr.reciptopia.reciptopiaserver.domain.model.Favorite;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FavoriteRepository extends BaseRepository<Favorite, Long> {
-
+    void deleteAllInBatchByOwnerId(Long ownerId);
 }
--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/SearchHistoryRepository.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/SearchHistoryRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SearchHistoryRepository extends BaseRepository<SearchHistory, Long> {
-
+    void deleteAllInBatchByOwnerId(Long ownerId);
 }
