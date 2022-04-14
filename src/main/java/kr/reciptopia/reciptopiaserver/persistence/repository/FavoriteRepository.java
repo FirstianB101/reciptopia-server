e0f0dc7eee8974218c7486dac64c3ec18f4ec644
tellang <tellang@daum.net>
2022-04-14 21:14:53 +0900

    Post의 id로 각각의 엔티티 삭제하는 메소드 추가
    단방향으로 변경으로 인한 하위 자원 삭제시 상위 자원 삭제기능 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/FavoriteRepository.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/FavoriteRepository.java
@@ -5,5 +5,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface FavoriteRepository extends BaseRepository<Favorite, Long> {
+
     void deleteAllInBatchByOwnerId(Long ownerId);
+
+    void deleteAllInBatchByPostId(Long postId);
 }
--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/RecipeRepository.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/RecipeRepository.java
@@ -5,5 +5,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RecipeRepository extends BaseRepository<Recipe, Long> {
-
+    void deleteAllInBatchByPostId(Long postId);
 }
