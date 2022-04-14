0f0c0fdb25939a0d6654986013dd02171d71f7a7
tellang <tellang@daum.net>
2022-04-14 21:18:21 +0900

    Comment의 id로 각각의 엔티티 삭제하는 메소드 추가
    단방향으로 변경으로 인한 하위 자원 삭제시 상위 자원 삭제기능 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/ReplyRepository.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/ReplyRepository.java
@@ -5,5 +5,8 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReplyRepository extends BaseRepository<Reply, Long> {
+
     void deleteAllInBatchByOwnerId(Long ownerId);
+
+    void deleteAllInBatchByCommentId(Long commentId);
 }
