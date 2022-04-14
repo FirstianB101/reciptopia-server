58ffa1cda802ef319f5d996f809ec9e7f4f84586
tellang <tellang@daum.net>
2022-04-14 15:59:21 +0900

    DTO에 누락된 Entity변환 메소드 추가
    `Entity` <-> `DTO` 변환 메소드 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/FavoriteDto.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/FavoriteDto.java
@@ -1,9 +1,13 @@
 package kr.reciptopia.reciptopiaserver.domain.dto;
 
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import javax.validation.constraints.NotNull;
 import kr.reciptopia.reciptopiaserver.domain.model.Favorite;
 import lombok.Builder;
 import lombok.With;
+import org.springframework.data.util.Streamable;
 
 public interface FavoriteDto {
 
@@ -20,6 +24,20 @@ public interface FavoriteDto {
                 Long postId) {
             this.ownerId = ownerId;
             this.postId = postId;
+        }
+
+        public Favorite asEntity(
+            Function<? super Favorite, ? extends Favorite> initialize) {
+            return initialize.apply(Favorite.builder()
+                .build());
+        }
+
+        public Favorite asEntity() {
+            return asEntity(noInit());
+        }
+
+        private <T> Function<? super T, ? extends T> noInit() {
+            return (arg) -> arg;
         }
     }
 
@@ -48,6 +66,12 @@ public interface FavoriteDto {
                 .ownerId(entity.getOwner().getId())
                 .postId(entity.getPost().getId())
                 .build();
+        }
+
+        public static List<Result> of(Streamable<Favorite> favorites) {
+            return favorites.stream()
+                .map(Result::of)
+                .collect(Collectors.toList());
         }
     }
 }
