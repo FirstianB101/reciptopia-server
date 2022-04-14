452194af1da5dcc1fbf364480f510f43bdaf5abf
tellang <tellang@daum.net>
2022-04-14 21:15:04 +0900

    사용하지 않는 메소드 삭제

--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/PostRepository.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/PostRepository.java
@@ -1,13 +1,10 @@
 package kr.reciptopia.reciptopiaserver.persistence.repository;
 
-import java.util.Optional;
 import kr.reciptopia.reciptopiaserver.domain.model.Post;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends BaseRepository<Post, Long> {
-
-	Optional<Post> findByOwnerIdAndRecipeId(Long ownerId, Long recipeId);
 
 	void deleteAllInBatchByOwnerId(Long ownerId);
 }
