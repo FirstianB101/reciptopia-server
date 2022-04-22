0847e0bd10ea691e109bdfc3f35280889778049a
tellang <tellang@daum.net>
2022-04-22 10:46:40 +0900

    연관관계 연쇄 삭제 오류 수정에 의한 삭제 방식 변경
    부모가 여렷인 자식간 관계에서 특정 부모가 다른 자식들을 갖고 있는 부모를 삭제시, 일부 부모가 다른 자식이 삭제가 되지 않았던 오류 수정으로 인한 변경
    `Reply`를 `AccountId` 또는 `CommentId`를 통해 삭제하는 방식에서 `@OnDelete`를 사용한 방식으로 변경

--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/ReplyRepository.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/ReplyRepository.java
@@ -6,7 +6,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReplyRepository extends BaseRepository<Reply, Long> {
 
-    void deleteAllInBatchByOwnerId(Long ownerId);
-
-    void deleteAllInBatchByCommentId(Long commentId);
 }
