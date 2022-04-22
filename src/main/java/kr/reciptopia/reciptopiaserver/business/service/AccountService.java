83f3078c048812e130070fde88b82ab5dea962dd
tellang <tellang@daum.net>
2022-04-22 10:53:37 +0900

    연관관계 연쇄 삭제 오류 수정에 의한 삭제 방식 변경
    부모가 여렷인 자식간 관계에서 특정 부모가 다른 자식들을 갖고 있는 부모를 삭제시, 일부 부모가 다른 자식이 삭제가 되지 않았던 오류 수정으로 인한 변경
    부모 자식간 연관관계 연쇄 삭제를 `@OnDelete`를 사용한 방식으로 변경함에 따라 불필요한 메소드 삭제

--- src/main/java/kr/reciptopia/reciptopiaserver/business/service/AccountService.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/business/service/AccountService.java
@@ -13,12 +13,6 @@ import kr.reciptopia.reciptopiaserver.business.service.helper.ServiceErrorHelper
 import kr.reciptopia.reciptopiaserver.domain.model.Account;
 import kr.reciptopia.reciptopiaserver.domain.model.UserRole;
 import kr.reciptopia.reciptopiaserver.persistence.repository.AccountRepository;
-import kr.reciptopia.reciptopiaserver.persistence.repository.CommentRepository;
-import kr.reciptopia.reciptopiaserver.persistence.repository.FavoriteRepository;
-import kr.reciptopia.reciptopiaserver.persistence.repository.PostLikeTagRepository;
-import kr.reciptopia.reciptopiaserver.persistence.repository.PostRepository;
-import kr.reciptopia.reciptopiaserver.persistence.repository.ReplyRepository;
-import kr.reciptopia.reciptopiaserver.persistence.repository.SearchHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -35,12 +29,6 @@ public class AccountService {
 
     private final PasswordEncoder passwordEncoder;
     private final AccountRepository accountRepository;
-    private final PostRepository postRepository;
-    private final CommentRepository commentRepository;
-    private final ReplyRepository replyRepository;
-    private final FavoriteRepository favoriteRepository;
-    private final SearchHistoryRepository searchHistoryRepository;
-    private final PostLikeTagRepository postLikeTagRepository;
     private final RepositoryHelper repoHelper;
     private final ServiceErrorHelper errorHelper;
     private final AccountAuthorizer accountAuthorizer;
@@ -91,12 +79,6 @@ public class AccountService {
         Account account = repoHelper.findAccountOrThrow(id);
         accountAuthorizer.requireByOneself(authentication, account);
 
-        postRepository.deleteAllInBatchByOwnerId(account.getId());
-        commentRepository.deleteAllInBatchByOwnerId(account.getId());
-        replyRepository.deleteAllInBatchByOwnerId(account.getId());
-        favoriteRepository.deleteAllInBatchByOwnerId(account.getId());
-        searchHistoryRepository.deleteAllInBatchByOwnerId(account.getId());
-        postLikeTagRepository.deleteAllInBatchByOwnerId(account.getId());
         accountRepository.delete(account);
     }
 
