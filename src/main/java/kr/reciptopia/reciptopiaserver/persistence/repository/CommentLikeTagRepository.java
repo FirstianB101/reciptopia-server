420677221bbe83293140f31d5d1f50b52fd32744
Kim EunSung <dmstjd365@naver.com>
2022-04-24 16:44:09 +0900

    CommentLikeTagRepository 추가

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/CommentLikeTagRepository.java
@@ -0,0 +1,9 @@
+package kr.reciptopia.reciptopiaserver.persistence.repository;
+
+import kr.reciptopia.reciptopiaserver.domain.model.CommentLikeTag;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentLikeTagRepository extends BaseRepository<CommentLikeTag, Long> {
+
+}
