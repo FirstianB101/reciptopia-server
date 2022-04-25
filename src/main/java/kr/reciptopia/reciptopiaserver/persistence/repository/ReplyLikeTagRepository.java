fe8e40d8be1ba70602cfde85c3d05c55b6b245ce
Kim EunSung <dmstjd365@naver.com>
2022-04-25 17:26:11 +0900

    ReplyLikeTagRepository 추가

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/ReplyLikeTagRepository.java
@@ -0,0 +1,9 @@
+package kr.reciptopia.reciptopiaserver.persistence.repository;
+
+import kr.reciptopia.reciptopiaserver.domain.model.ReplyLikeTag;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReplyLikeTagRepository extends BaseRepository<ReplyLikeTag, Long> {
+
+}
