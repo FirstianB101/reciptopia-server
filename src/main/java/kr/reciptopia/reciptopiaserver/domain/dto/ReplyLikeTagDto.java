1bba3acc008d4ddbb6648732aac84f0874e5d446
Kim EunSung <dmstjd365@naver.com>
2022-04-25 17:25:45 +0900

    ReplyLikeTagDto 추가

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/ReplyLikeTagDto.java
@@ -0,0 +1,76 @@
+package kr.reciptopia.reciptopiaserver.domain.dto;
+
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import javax.validation.constraints.NotNull;
+import kr.reciptopia.reciptopiaserver.domain.model.ReplyLikeTag;
+import lombok.Builder;
+import lombok.With;
+import org.springframework.data.util.Streamable;
+
+public interface ReplyLikeTagDto {
+
+	@With
+	record Create(
+		Long ownerId, Long replyId) {
+
+		@Builder
+		public Create(
+			@NotNull
+				Long ownerId,
+
+			@NotNull
+				Long replyId) {
+			this.ownerId = ownerId;
+			this.replyId = replyId;
+		}
+
+		public ReplyLikeTag asEntity(
+			Function<? super ReplyLikeTag, ? extends ReplyLikeTag> initialize) {
+			return initialize.apply(ReplyLikeTag.builder().build());
+		}
+
+		public ReplyLikeTag asEntity() {
+			return asEntity(noInit());
+		}
+
+		private <T> Function<? super T, ? extends T> noInit() {
+			return (arg) -> arg;
+		}
+	}
+
+	@With
+	record Result(
+		Long id, Long ownerId, Long replyId) {
+
+		@Builder
+		public Result(
+			@NotNull
+				Long id,
+
+			@NotNull
+				Long ownerId,
+
+			@NotNull
+				Long replyId) {
+			this.id = id;
+			this.ownerId = ownerId;
+			this.replyId = replyId;
+		}
+
+		public static Result of(ReplyLikeTag entity) {
+			return Result.builder()
+				.id(entity.getId())
+				.ownerId(entity.getOwner().getId())
+				.replyId(entity.getReply().getId())
+				.build();
+		}
+
+		public static List<Result> of(Streamable<ReplyLikeTag> entities) {
+			return entities.stream()
+				.map(Result::of)
+				.collect(Collectors.toList());
+		}
+	}
+}
