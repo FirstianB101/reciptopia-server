fc1af4d7d534a855abdbccd23ac685be2a75cf26
tellang <tellang@daum.net>
2022-05-11 01:53:00 +0900

    Recipe 조건 검색 API 추가
    패터미터의 `MainIngredient`의 `Name`이 모두 포함된 `Recipe`를 `SubIngredient`의 `Name`이 포함된 순으로 정렬하여 반환하는 API 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/business/service/RecipeService.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/business/service/RecipeService.java
@@ -1,14 +1,19 @@
 package kr.reciptopia.reciptopiaserver.business.service;
 
+import static kr.reciptopia.reciptopiaserver.domain.dto.RecipeDto.Bulk;
 import static kr.reciptopia.reciptopiaserver.domain.dto.RecipeDto.Create;
 import static kr.reciptopia.reciptopiaserver.domain.dto.RecipeDto.Result;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import kr.reciptopia.reciptopiaserver.business.service.authorizer.RecipeAuthorizer;
 import kr.reciptopia.reciptopiaserver.business.service.helper.RepositoryHelper;
+import kr.reciptopia.reciptopiaserver.business.service.spec.searchcondition.RecipeSearchCondition;
 import kr.reciptopia.reciptopiaserver.domain.model.Post;
 import kr.reciptopia.reciptopiaserver.domain.model.Recipe;
 import kr.reciptopia.reciptopiaserver.persistence.repository.RecipeRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,6 +39,23 @@ public class RecipeService {
 
     public Result read(Long id) {
         return Result.of(repoHelper.findRecipeOrThrow(id));
+    }
+
+    public Bulk.Result search(RecipeSearchCondition condition, Pageable pageable) {
+        List<Recipe> recipes = RecipeRepository.findAll().stream()
+            .filter(condition.mainIngredientNames().isEmpty() ?
+                recipe -> true :
+                recipe -> recipe.hasAllMainIngredients(condition.mainIngredientNames()))
+            .distinct()
+            .sorted(condition.subIngredientNames().isEmpty() ?
+                (o1, o2) -> (int) (o1.getId() - o2.getId()) :
+                (o1, o2) -> o2.countIncludedSubIngredients(condition.subIngredientNames())
+                    - o1.countIncludedSubIngredients(condition.subIngredientNames()))
+            .collect(Collectors.toList());
+        return Bulk.Result.of(recipes.subList(
+            (int) pageable.getOffset(),
+            (int) Math.min((pageable.getOffset() + pageable.getPageSize()),
+                recipes.size() - pageable.getOffset())));
     }
 
     @Transactional
