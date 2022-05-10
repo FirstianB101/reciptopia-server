8272681fc24cde1c2da5dbdcc94d2b8c7258143d
tellang <tellang@daum.net>
2022-05-11 01:45:14 +0900

    BulkDto 추가
    - `Recipe`의 Bulk ResultDto 추가
    - Sorting을 고려하여 `LinkedHashMap`을 사용하여 구현

--- src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/RecipeDto.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/RecipeDto.java
@@ -1,11 +1,16 @@
 package kr.reciptopia.reciptopiaserver.domain.dto;
 
+import java.util.Collection;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import kr.reciptopia.reciptopiaserver.domain.model.Recipe;
 import lombok.Builder;
+import lombok.Singular;
 import lombok.With;
 import org.springframework.data.util.Streamable;
 
@@ -65,4 +70,35 @@ public interface RecipeDto {
                 .collect(Collectors.toList());
         }
     }
+
+    interface Bulk {
+
+        @With
+        record Result(
+            Map<Long, RecipeDto.Result> recipes) {
+
+            @Builder
+            public Result(
+                @NotEmpty
+                @Singular
+                    Map<Long, RecipeDto.Result> recipes
+            ) {
+                this.recipes = recipes;
+            }
+
+            public static Result of(Collection<Recipe> recipes) {
+                return Result.builder()
+                    .recipes((Map<? extends Long, ? extends RecipeDto.Result>) recipes.stream()
+                        .map(RecipeDto.Result::of)
+                        .collect(
+                            Collectors.toMap(
+                                RecipeDto.Result::id,
+                                result -> result,
+                                (x, y) -> y,
+                                LinkedHashMap::new)))
+                    .build();
+            }
+        }
+    }
+
 }
