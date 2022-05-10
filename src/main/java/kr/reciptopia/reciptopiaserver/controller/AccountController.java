bf1b7b6fb04536b58d80e86cbd5a93b0bd3ee2f0
tellang <tellang@daum.net>
2022-05-10 19:29:36 +0900

    검색 파라미터 삭제
    `Account`의 검색 파라미터 변경 및 쿼리 방식 변경에 따른 변경사항 적용

--- src/main/java/kr/reciptopia/reciptopiaserver/controller/AccountController.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/controller/AccountController.java
@@ -1,18 +1,15 @@
 package kr.reciptopia.reciptopiaserver.controller;
 
+import static kr.reciptopia.reciptopiaserver.domain.dto.AccountDto.Bulk;
 import static kr.reciptopia.reciptopiaserver.domain.dto.AccountDto.CheckDuplicationResult;
 import static kr.reciptopia.reciptopiaserver.domain.dto.AccountDto.Create;
 import static kr.reciptopia.reciptopiaserver.domain.dto.AccountDto.Result;
 import static kr.reciptopia.reciptopiaserver.domain.dto.AccountDto.Update;
 
-import java.util.List;
 import javax.validation.Valid;
 import kr.reciptopia.reciptopiaserver.business.service.AccountService;
-import kr.reciptopia.reciptopiaserver.business.service.spec.AccountSpecs;
-import kr.reciptopia.reciptopiaserver.domain.model.Account;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
@@ -22,7 +19,6 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -45,24 +41,8 @@ public class AccountController {
     }
 
     @GetMapping("/accounts")
-    public List<Result> search(
-        @RequestParam(required = false) Long postId,
-        @RequestParam(required = false) Long commentId,
-        @RequestParam(required = false) Long replyId,
-        Pageable pageable
-    ) {
-        Specification<Account> spec = null;
-        if (postId != null) {
-            spec = AccountSpecs.hasPost(postId).and(spec);
-        }
-        if (commentId != null) {
-            spec = AccountSpecs.hasComment(commentId).and(spec);
-        }
-        if (replyId != null) {
-            spec = AccountSpecs.hasReply(replyId).and(spec);
-        }
-
-        return service.search(spec, pageable);
+    public Bulk.Result search(Pageable pageable) {
+        return service.search(pageable);
     }
 
     @DeleteMapping("/accounts/{id}")
