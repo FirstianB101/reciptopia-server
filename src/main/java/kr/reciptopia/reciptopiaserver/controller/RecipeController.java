bd9fedba8f5a403a6d688170cf6b5b2da17d8461
tellang <tellang@daum.net>
2022-05-11 01:53:00 +0900

    Recipe 조건 검색 API 추가
    패터미터의 `MainIngredient`의 `Name`이 모두 포함된 `Recipe`를 `SubIngredient`의 `Name`이 포함된 순으로 정렬하여 반환하는 API 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/controller/RecipeController.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/controller/RecipeController.java
@@ -1,11 +1,16 @@
 package kr.reciptopia.reciptopiaserver.controller;
 
+import static kr.reciptopia.reciptopiaserver.domain.dto.RecipeDto.Bulk;
+import static kr.reciptopia.reciptopiaserver.domain.dto.RecipeDto.Create;
+import static kr.reciptopia.reciptopiaserver.domain.dto.RecipeDto.Result;
 import static org.springframework.http.HttpStatus.NO_CONTENT;
 
+import java.util.Set;
 import javax.validation.Valid;
 import kr.reciptopia.reciptopiaserver.business.service.RecipeService;
-import kr.reciptopia.reciptopiaserver.domain.dto.RecipeDto;
+import kr.reciptopia.reciptopiaserver.business.service.spec.searchcondition.RecipeSearchCondition;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +40,19 @@ public class RecipeController {
     @GetMapping("/post/recipes/{id}")
     public RecipeDto.Result get(@PathVariable Long id) {
         return service.read(id);
+    }
+
+    @GetMapping("/post/recipes")
+    public Bulk.Result search(
+        @RequestParam(required = false) Set<String> mainIngredientNames,
+        @RequestParam(required = false) Set<String> subIngredientNames,
+        Pageable pageable
+    ) {
+        RecipeSearchCondition condition = RecipeSearchCondition.builder()
+            .mainIngredientNames(mainIngredientNames)
+            .subIngredientNames(subIngredientNames)
+            .build();
+        return service.search(condition, pageable);
     }
 
     @DeleteMapping("/post/recipes/{id}")
