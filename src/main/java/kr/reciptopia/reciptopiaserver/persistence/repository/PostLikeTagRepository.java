eff3b0e6c40894872626b4c1f39b46345a22509b
tellang <tellang@daum.net>
2022-05-12 00:52:45 +0900

    PostLikeTagRepository에 메소드 추가
    `PostId` 로 해당 `Post` 에 속한 `PostLikeTag` 를 검색할 수 있는 메소드 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/PostLikeTagRepository.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/PostLikeTagRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostLikeTagRepository extends BaseRepository<PostLikeTag, Long> {
 
+    int countByPostId(Long postId);
 }
