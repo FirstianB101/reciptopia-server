8cbe3552e97ddf4e42819e8e395c3e97485cc3d8
Kim EunSung <dmstjd365@naver.com>
2022-05-11 19:35:27 +0900

    BulkDto 추가
    Sorting을 위한 LinkedHashMap 사용

--- src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/CommentDto.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/CommentDto.java
@@ -1,16 +1,50 @@
 package kr.reciptopia.reciptopiaserver.domain.dto;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import kr.reciptopia.reciptopiaserver.domain.model.Comment;
 import lombok.Builder;
+import lombok.Singular;
 import lombok.With;
+import org.springframework.data.domain.Page;
 import org.springframework.data.util.Streamable;
 
 public interface CommentDto {
+
+    interface Bulk {
+
+        @With
+        record Result(Map<Long, CommentDto.Result> comments) {
+
+            @Builder
+            public Result(
+                @NotEmpty
+                @Singular
+                    Map<Long, CommentDto.Result> comments
+            ) {
+                this.comments = comments;
+            }
+
+            public static Result of(Page<Comment> comments) {
+                return Result.builder()
+                    .comments((Map<? extends Long, ? extends CommentDto.Result>) comments.stream()
+                        .map(CommentDto.Result::of)
+                        .collect(
+                            Collectors.toMap(
+                                CommentDto.Result::id,
+                                result -> result,
+                                (x, y) -> y,
+                                LinkedHashMap::new)))
+                    .build();
+            }
+        }
+    }
 
     @With
     record Create(
