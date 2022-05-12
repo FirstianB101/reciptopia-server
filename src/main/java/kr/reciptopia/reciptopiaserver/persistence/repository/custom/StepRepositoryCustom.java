0e3fe0847d5fa75e83fbaa38362456f7f77a635d
tellang <tellang@daum.net>
2022-05-12 14:32:34 +0900

    Step 조건 검색을 위한 QueryDSL 커스텀 리포지토리 추가

---
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/custom/StepRepositoryCustom.java
@@ -0,0 +1,11 @@
+package kr.reciptopia.reciptopiaserver.persistence.repository.custom;
+
+import kr.reciptopia.reciptopiaserver.business.service.spec.searchcondition.StepSearchCondition;
+import kr.reciptopia.reciptopiaserver.domain.model.Step;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+public interface StepRepositoryCustom {
+
+    PageImpl<Step> search(StepSearchCondition stepSearchCondition, Pageable pageable);
+}
