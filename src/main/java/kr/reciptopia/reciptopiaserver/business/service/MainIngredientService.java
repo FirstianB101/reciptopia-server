f7dc4d537a123c1b3b2da04d5812c32940f2107d
tellang <tellang@daum.net>
2022-05-16 22:32:05 +0900

    MainIngredient 조건 검색 API 추가 및 수정
    - 검색 API 가 `Bulk` 형식의 반환값을 갖게 됨에 따라 비즈니스 로직도 `Bulk` 형식의 반환값을 갖게 변경

--- src/main/java/kr/reciptopia/reciptopiaserver/business/service/MainIngredientService.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/business/service/MainIngredientService.java
@@ -8,13 +8,13 @@ import static kr.reciptopia.reciptopiaserver.domain.dto.MainIngredientDto.Update
 import java.util.List;
 import kr.reciptopia.reciptopiaserver.business.service.authorizer.IngredientAuthorizer;
 import kr.reciptopia.reciptopiaserver.business.service.helper.RepositoryHelper;
+import kr.reciptopia.reciptopiaserver.business.service.searchcondition.MainIngredientSearchCondition;
 import kr.reciptopia.reciptopiaserver.domain.model.MainIngredient;
 import kr.reciptopia.reciptopiaserver.domain.model.Recipe;
 import kr.reciptopia.reciptopiaserver.persistence.repository.MainIngredientRepository;
+import kr.reciptopia.reciptopiaserver.persistence.repository.implementaion.MainIngredientRepositoryImpl;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,6 +25,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class MainIngredientService {
 
     private final MainIngredientRepository mainIngredientRepository;
+    private final MainIngredientRepositoryImpl mainIngredientRepositoryImpl;
     private final RepositoryHelper repoHelper;
     private final IngredientAuthorizer ingredientAuthorizer;
 
@@ -42,9 +43,8 @@ public class MainIngredientService {
         return Result.of(repoHelper.findMainIngredientOrThrow(id));
     }
 
-    public List<Result> search(Specification<MainIngredient> spec, Pageable pageable) {
-        Page<MainIngredient> entities = mainIngredientRepository.findAll(spec, pageable);
-        return Result.of(entities);
+    public Bulk.Result search(MainIngredientSearchCondition condition, Pageable pageable) {
+        return Bulk.Result.of(mainIngredientRepositoryImpl.search(condition, pageable));
     }
 
     @Transactional
--- src/main/java/kr/reciptopia/reciptopiaserver/controller/MainIngredientController.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/controller/MainIngredientController.java
@@ -1,12 +1,15 @@
 package kr.reciptopia.reciptopiaserver.controller;
 
+import static kr.reciptopia.reciptopiaserver.domain.dto.MainIngredientDto.Bulk;
 import static kr.reciptopia.reciptopiaserver.domain.dto.MainIngredientDto.Create;
 import static kr.reciptopia.reciptopiaserver.domain.dto.MainIngredientDto.Result;
 import static kr.reciptopia.reciptopiaserver.domain.dto.MainIngredientDto.Update;
 
 import javax.validation.Valid;
 import kr.reciptopia.reciptopiaserver.business.service.MainIngredientService;
+import kr.reciptopia.reciptopiaserver.business.service.searchcondition.MainIngredientSearchCondition;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.validation.annotation.Validated;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -37,6 +41,18 @@ public class MainIngredientController {
     public Result get(@PathVariable Long id) {
         return service.read(id);
     }
+
+    @GetMapping("/post/recipe/mainIngredients")
+    public Bulk.Result search(
+        @RequestParam(required = false) Long recipeId,
+        Pageable pageable) {
+        MainIngredientSearchCondition mainIngredientSearchCondition = MainIngredientSearchCondition.builder()
+            .recipeId(recipeId)
+            .build();
+
+        return service.search(mainIngredientSearchCondition, pageable);
+    }
+
 
     @PatchMapping("/post/recipe/mainIngredients/{id}")
     public Result patch(@PathVariable Long id,
