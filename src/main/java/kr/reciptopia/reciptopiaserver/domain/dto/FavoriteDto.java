3d37ca5062ba0877796232661b1ceacb1418d5de
Kim EunSung <dmstjd365@naver.com>
2022-05-19 16:00:19 +0900

    Favorite의 Bulk.Result Dto 추가
    - Sorting을 위해 `LinkedHashMap` 사용

--- src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/FavoriteDto.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/FavoriteDto.java
@@ -1,15 +1,49 @@
 package kr.reciptopia.reciptopiaserver.domain.dto;
 
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import kr.reciptopia.reciptopiaserver.domain.model.Favorite;
 import lombok.Builder;
+import lombok.Singular;
 import lombok.With;
+import org.springframework.data.domain.Page;
 import org.springframework.data.util.Streamable;
 
 public interface FavoriteDto {
+
+    interface Bulk {
+
+        @With
+        record Result(Map<Long, FavoriteDto.Result> favorites) {
+
+            @Builder
+            public Result(
+                @NotEmpty
+                @Singular
+                    Map<Long, FavoriteDto.Result> favorites) {
+                this.favorites = favorites;
+            }
+
+            public static Result of(Page<Favorite> favorites) {
+                return Result.builder()
+                    .favorites(
+                        (Map<? extends Long, ? extends FavoriteDto.Result>) favorites.stream()
+                            .map(FavoriteDto.Result::of)
+                            .collect(
+                                Collectors.toMap(
+                                    FavoriteDto.Result::id,
+                                    result -> result,
+                                    (x, y) -> y,
+                                    LinkedHashMap::new)))
+                    .build();
+            }
+        }
+    }
 
     @With
     record Create(
