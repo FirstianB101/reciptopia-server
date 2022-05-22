f735e681558ca83c6a3454d6c8f8e82ca1cf96cc
tellang <tellang@daum.net>
2022-05-22 22:15:11 +0900

    PostSearchCondition에 메소드 추가
    - `PostSearchCondition` 에서 `RecipeSearchCondition` 를 추출하는 메소드 추가
    - `ids` 만을 바꾼 `PostSearchCondition` 를 생성하는 메소드 추가
    - `RecipeSearchCondition` 를 기반으로 `PostSearchCondition` 를 다시 만들어주는 메소드 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/business/service/searchcondition/PostSearchCondition.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/business/service/searchcondition/PostSearchCondition.java
@@ -22,4 +22,35 @@ public record PostSearchCondition(
 		this.ids = ids == null ? new ArrayList<>() : ids;
 	}
 
+
+	public static RecipeSearchCondition getRecipeSearchCondition(
+		PostSearchCondition postSearchCondition) {
+		return RecipeSearchCondition.builder()
+			.mainIngredientNames(postSearchCondition.mainIngredientNames())
+			.subIngredientNames(postSearchCondition.subIngredientNames()).build();
+	}
+
+	private static PostSearchCondition withIds(PostSearchCondition condition, List<Long> ids) {
+		return PostSearchCondition.builder()
+			.ownerId(condition.ownerId())
+			.titleLike(condition.titleLike())
+			.mainIngredientNames(condition.mainIngredientNames())
+			.subIngredientNames(condition.subIngredientNames())
+			.ids(ids)
+			.build();
+	}
+
+	public static PostSearchCondition updateConditionWithRecipeCondition(
+		PostSearchCondition condition,
+		RecipeSearchCondition recipeSearchCondition, List<Long> postIds) {
+		if (!recipeSearchCondition.isEmpty()) {
+			List<Long> conditionIds = condition.ids();
+			if (!conditionIds.isEmpty())
+				conditionIds.retainAll(postIds);
+			else
+				conditionIds = postIds;
+			condition = withIds(condition, conditionIds);
+		}
+		return condition;
+	}
 }
