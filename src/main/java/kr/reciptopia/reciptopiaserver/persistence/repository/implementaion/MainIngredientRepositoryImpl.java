1362d1fa333402fa7fbedf15b13bcbf0e4ad10d0
tellang <tellang@daum.net>
2022-05-25 14:43:30 +0900

    MainIngredient 조건 검색을 QueryDSL에 추가
    아래에 해당하는 검색 조건 추가
    - Post의 id와 MainIngredient가 속한 Post의 id가 일치한 MainIngredient
    - Post의 ids의 id와 MainIngredient가 속한 Post의 id가 일치한 MainIngredient들
    - Recipe의 id와 MainIngredient가 속한 Post의 id가 일치한 MainIngredient
    - Recipe의 ids의 id와 MainIngredient가 속한 Recipe의 id가 일치한 MainIngredient들

--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/implementaion/MainIngredientRepositoryImpl.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/implementaion/MainIngredientRepositoryImpl.java
@@ -1,10 +1,13 @@
 package kr.reciptopia.reciptopiaserver.persistence.repository.implementaion;
 
 import static kr.reciptopia.reciptopiaserver.domain.model.QMainIngredient.mainIngredient;
+import static kr.reciptopia.reciptopiaserver.domain.model.QPost.post;
 
+import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
 import kr.reciptopia.reciptopiaserver.business.service.searchcondition.MainIngredientSearchCondition;
 import kr.reciptopia.reciptopiaserver.config.querydsl.PagingUtil;
 import kr.reciptopia.reciptopiaserver.domain.model.MainIngredient;
@@ -23,15 +26,38 @@ public class MainIngredientRepositoryImpl implements MainIngredientRepositoryCus
 	public PageImpl<Tuple> search(
 		MainIngredientSearchCondition mainIngredientSearchCondition,
 		Pageable pageable) {
-		JPAQuery<MainIngredient> query = queryFactory
-			.selectFrom(mainIngredient)
-			.where(eqRecipeId(mainIngredientSearchCondition.recipeId()));
+		JPAQuery<Tuple> ingredientWithPostId = queryFactory
+			.from(post, mainIngredient)
+			.select(post.id, mainIngredient)
+			.where(
+				post.eq(mainIngredient.recipe.post),
+				eqRecipeId(mainIngredientSearchCondition.recipeId()),
+				eqPostId(mainIngredientSearchCondition.postId()),
+				inIds(mainIngredientSearchCondition.ids()),
+				inRecipeIds(mainIngredientSearchCondition.recipeIds()),
+				inPostIds(mainIngredientSearchCondition.postIds())
+			);
 
-		return pagingUtil.getPageImpl(pageable, query, MainIngredient.class);
+		return pagingUtil.getPageImpl(pageable, ingredientWithPostId, MainIngredient.class);
 	}
 
 	private BooleanExpression eqRecipeId(Long recipeId) {
 		return recipeId == null ? null : mainIngredient.recipe.id.eq(recipeId);
 	}
 
+	private BooleanExpression eqPostId(Long postId) {
+		return postId == null ? null : mainIngredient.recipe.post.id.eq(postId);
+	}
+
+	private BooleanExpression inIds(List<Long> ids) {
+		return ids.isEmpty() ? null : mainIngredient.id.in(ids);
+	}
+
+	private BooleanExpression inRecipeIds(List<Long> recipeIds) {
+		return recipeIds.isEmpty() ? null : mainIngredient.recipe.id.in(recipeIds);
+	}
+
+	private BooleanExpression inPostIds(List<Long> postIds) {
+		return postIds.isEmpty() ? null : mainIngredient.recipe.post.id.in(postIds);
+	}
 }
