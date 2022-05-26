364162acbe1fc772013ba185b56c19a62c7afa96
tellang <tellang@daum.net>
2022-05-26 16:42:04 +0900

    Account의 조건 검색을 위한 QueryDSL 커스텀 리포지토리 수정
    `AccountSearchCondition` 을 포함한 검색을 할 수 있도록 매개변수 수정

--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/custom/AccountRepositoryCustom.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/custom/AccountRepositoryCustom.java
@@ -1,10 +1,10 @@
 package kr.reciptopia.reciptopiaserver.persistence.repository.custom;
 
-import kr.reciptopia.reciptopiaserver.domain.model.Account;
+import kr.reciptopia.reciptopiaserver.business.service.searchcondition.AccountSearchCondition;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
 public interface AccountRepositoryCustom {
 
-    PageImpl<Account> search(Pageable pageable);
+    PageImpl<?> search(AccountSearchCondition condition, Pageable pageable);
 }
