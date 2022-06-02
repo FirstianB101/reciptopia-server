4d31a9ae8ab41b394f251547e7a7a6cf000a6666
tellang <tellang@daum.net>
2022-06-02 16:24:55 +0900

    SearchHistory의 잘못된 검색 로직 수정
    누락된 `ownerId` 를 통한 검색 로직 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/SearchHistoryRepository.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/persistence/repository/SearchHistoryRepository.java
@@ -1,9 +1,12 @@
 package kr.reciptopia.reciptopiaserver.persistence.repository;
 
 import kr.reciptopia.reciptopiaserver.domain.model.SearchHistory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SearchHistoryRepository extends BaseRepository<SearchHistory, Long> {
 
+    Page<SearchHistory> findAllByOwnerId(Long ownerId, Pageable pageable);
 }
