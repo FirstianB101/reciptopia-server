b18376d051b0a0152cb2aec74ae11282ff1e5e48
Kim EunSung <dmstjd365@naver.com>
2022-06-08 00:39:23 +0900

    UploadFileAuthHelper에 StepImg 추가

--- src/test/java/kr/reciptopia/reciptopiaserver/helper/auth/UploadFileAuthHelper.java
+++ src/test/java/kr/reciptopia/reciptopiaserver/helper/auth/UploadFileAuthHelper.java
@@ -3,6 +3,7 @@ package kr.reciptopia.reciptopiaserver.helper.auth;
 import kr.reciptopia.reciptopiaserver.business.service.JwtService;
 import kr.reciptopia.reciptopiaserver.domain.model.AccountProfileImg;
 import kr.reciptopia.reciptopiaserver.domain.model.PostImg;
+import kr.reciptopia.reciptopiaserver.domain.model.StepImg;
 import kr.reciptopia.reciptopiaserver.domain.model.UploadFile;
 import kr.reciptopia.reciptopiaserver.helper.auth.error.exception.UploadFileInvalidTypeException;
 import org.springframework.stereotype.Component;
@@ -20,6 +21,10 @@ public class UploadFileAuthHelper extends AuthHelper {
 		}
 		if (uploadFile instanceof PostImg) {
 			return generateToken(((PostImg) uploadFile).getPost().getOwner());
+		}
+		if (uploadFile instanceof StepImg) {
+			return generateToken(
+				((StepImg) uploadFile).getStep().getRecipe().getPost().getOwner());
 		}
 
 		throw new UploadFileInvalidTypeException();
