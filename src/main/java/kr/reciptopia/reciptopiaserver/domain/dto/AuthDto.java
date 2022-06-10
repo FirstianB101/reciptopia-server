561a1f98b97dfc244389348e0e65976b128155c1
tellang <tellang@daum.net>
2022-06-10 14:48:44 +0900

    AuthDto.GenerateToken의 Bean Validation 추가
    - `password` 의 길이에 대한 `Bean Validation` 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/AuthDto.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/AuthDto.java
@@ -3,12 +3,14 @@ package kr.reciptopia.reciptopiaserver.domain.dto;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import lombok.Builder;
 
 public interface AuthDto {
 
     record GenerateToken(
-        @NotEmpty @Email String email, @NotEmpty String password) {
+        @NotEmpty @Email String email,
+        @NotEmpty @Size(min = 8, max = 16, message = "password는 8 ~ 16자 이여야 합니다!") String password) {
 
         @Builder
         public GenerateToken {
