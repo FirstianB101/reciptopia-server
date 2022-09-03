216f32b539bb2ac0720ec7d6f46760a842fa6f96
tellang <tellang@daum.net>
2022-09-03 15:10:43 +0900

    PostLikeTagHelper 메소드 변경
    `aPostLikeTagCreateDto` 를 특정 엔티티의 `Id` 로 생성이 가능하도록 수정

--- src/test/java/kr/reciptopia/reciptopiaserver/helper/PostLikeTagHelper.java
+++ src/test/java/kr/reciptopia/reciptopiaserver/helper/PostLikeTagHelper.java
@@ -1,37 +1,55 @@
 package kr.reciptopia.reciptopiaserver.helper;
 
+import static kr.reciptopia.reciptopiaserver.domain.dto.PostLikeTagDto.Create;
+import static kr.reciptopia.reciptopiaserver.domain.dto.PostLikeTagDto.Result;
 import static kr.reciptopia.reciptopiaserver.helper.AccountHelper.anAccount;
 import static kr.reciptopia.reciptopiaserver.helper.PostHelper.aPost;
-import kr.reciptopia.reciptopiaserver.domain.dto.PostLikeTagDto.Create;
-import kr.reciptopia.reciptopiaserver.domain.dto.PostLikeTagDto.Result;
+
+import java.util.function.Function;
 import kr.reciptopia.reciptopiaserver.domain.model.PostLikeTag;
 
 public class PostLikeTagHelper {
 
 	private static final Long ARBITRARY_ID = 0L;
-	private static final Long ARBITRARY_OWNER_ID = 0L;
-	private static final Long ARBITRARY_POST_ID = 0L;
+    private static final Long ARBITRARY_OWNER_ID = 0L;
+    private static final Long ARBITRARY_POST_ID = 0L;
 
-	public static PostLikeTag aPostLikeTag() {
-		return PostLikeTag.builder()
-			.owner(anAccount().withId(ARBITRARY_OWNER_ID))
-			.post(aPost().withId(ARBITRARY_POST_ID))
-			.build()
-			.withId(ARBITRARY_ID);
-	}
+    public static PostLikeTag aPostLikeTag() {
+        return PostLikeTag.builder()
+            .owner(anAccount().withId(ARBITRARY_OWNER_ID))
+            .post(aPost().withId(ARBITRARY_POST_ID))
+            .build()
+            .withId(ARBITRARY_ID);
+    }
 
-	public static Create aPostLikeTagCreateDto() {
-		return Create.builder()
-			.ownerId(ARBITRARY_OWNER_ID)
-			.postId(ARBITRARY_POST_ID)
-			.build();
-	}
+    public static Create aPostLikeTagCreateDto(
+        Function<? super Create, ? extends Create> initialize) {
+        Create createDto = Create.builder()
+            .build();
 
-	public static Result aPostLikeTagResultDto() {
-		return Result.builder()
-			.ownerId(ARBITRARY_OWNER_ID)
-			.postId(ARBITRARY_POST_ID)
-			.build();
-	}
+        createDto = initialize.apply(createDto);
+        return Create.builder()
+            .ownerId(getNonNull(createDto.ownerId(), ARBITRARY_OWNER_ID))
+            .postId(getNonNull(createDto.postId(), ARBITRARY_POST_ID))
+            .build();
+    }
 
+    public static Create aPostLikeTagCreateDto() {
+        return aPostLikeTagCreateDto(noInit());
+    }
+
+    public static Result aPostLikeTagResultDto() {
+        return Result.builder()
+            .ownerId(ARBITRARY_OWNER_ID)
+            .postId(ARBITRARY_POST_ID)
+            .build();
+    }
+
+    private static <T> Function<? super T, ? extends T> noInit() {
+        return (arg) -> arg;
+    }
+
+    private static Long getNonNull(Long inputValue, Long arbitraryValue) {
+        return inputValue == null ? arbitraryValue : inputValue;
+    }
 }
