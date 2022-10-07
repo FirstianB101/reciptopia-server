23dbbe97fae54dfa32b806edb5bce442042014d2
tellang <tellang@daum.net>
2022-10-07 13:01:52 +0900

    SubIngredientDto.Create.Single 의 잘못된 Bean Validation 수정 및 추가
    - 잘못된 `Bean Validation` 애노테이션 위치 수정
      * `Bean Validation` 위치 변경에 따른 생성자 생략
      * 생성자 생략에 따른 Builder 애노테이션의 위치 수정
    - `name` 과 `detail` 필드의 검증 애노테이션 변경 및 추가
      * `white space` 로만 이루어 지는것이 적절하지 않은 필드에 대해 해당 `Bean Validation` 추가
      * 해당 필드의 길이에 대한 `Bean Validation` 추가

--- src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/SubIngredientDto.java
+++ src/main/java/kr/reciptopia/reciptopiaserver/domain/dto/SubIngredientDto.java
@@ -7,8 +7,10 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import kr.reciptopia.reciptopiaserver.domain.model.SubIngredient;
 import lombok.Builder;
 import lombok.Singular;
@@ -140,24 +142,12 @@ public interface SubIngredientDto {
         }
 
         @With
+        @Builder
         record Single(
-            Long recipeId, String name, String detail
+            @NotNull Long recipeId,
+            @NotBlank @Size(min = 1, max = 20, message = "name은 1 ~ 20자 이여야 합니다!") String name,
+            @NotBlank @Size(min = 1, max = 50, message = "detail은 1 ~ 50자 이여야 합니다!") String detail
         ) {
-
-            @Builder
-            public Single(
-                @NotNull
-                    Long recipeId,
-
-                @NotEmpty
-                    String name,
-
-                @NotEmpty
-                    String detail) {
-                this.recipeId = recipeId;
-                this.name = name;
-                this.detail = detail;
-            }
 
             public SubIngredient asEntity(
                 Function<? super SubIngredient, ? extends SubIngredient> initialize) {
